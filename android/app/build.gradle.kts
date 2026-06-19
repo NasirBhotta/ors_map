375cd7988b8f8f1e1989b6db807fbb6a4380139f
Nasir Bhutta <me.bhotta@gmail.com>
2026-06-20 01:27:29 +0500

    updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -56,5 +56,5 @@ flutter {
     source = "../.."
 }
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")  // ✅ 2.0.4 → 2.1.4
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")  
 }
